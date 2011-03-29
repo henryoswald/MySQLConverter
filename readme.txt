60bfb3452b38c69827ef3f72e1cf984413b41a35
Henry Oswald <henry.oswald@gmail.com>
2011-03-29 20:55:14 +0100

    does the basics which I need

--- readme.txt
+++ readme.txt
@@ -1,3 +1,5 @@
 Converts MySQLWorkbench SQL to oracle SQL. Fairly basic things like VARCHAR becomes VARCAR2.
 
 Things like number range in oracle will need to be configured manually.
+
+By default it produces SQL for oracle databases that have already been created.
--- src/Converter.java
+++ src/Converter.java
@@ -1,42 +1,54 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+
+import java.io.*;
 import java.util.*;
 
 
 public class Converter {
 
-	/**
-	 * @param args
-	 */
+
 	public static void main(String[] args) {
 		
 
 		try {
 			StringBuffer sqlBuf = new StringBuffer();
-			Scanner inputFile = new Scanner(new File("DataFile.txt"));
+			Scanner inputFile = new Scanner(new File("test.sql"));
 			
 			//put entire SQL into string buffer
 			while(inputFile.hasNextLine()) {
 				sqlBuf.append(inputFile.nextLine());
+				sqlBuf.append("\n");
 			}
 			
 			//convert to String
 			String sql = sqlBuf.toString();
 			
-			sql = sql.replaceAll("'","\"");
+			sql = sql.replaceAll("`","\"");
 			sql = sql.replaceAll("VARCHAR","VARCHAR2");
 			sql = sql.replaceAll("NOT NULL","NOT NULL ENABLE");
-			sql = sql.replaceAll("INT","NUMBER(10,0)");
+			sql = sql.replaceAll(" INT "," NUMBER ");
 			sql = sql.replaceAll("ENGINE = \\w*;",";");//remove the engine type
-			sql = sql.replaceAll("ENGINE = \\w*;",";");//remove the engine type
-			sql = sql.replaceAll("IF NOT EXISTS `\\w*`\\.","");//remove the engine type
+			sql = sql.replaceAll("IF NOT EXISTS .\\w*.\\.","");//remove the engine type
+			sql = sql.replaceAll("DATETIME;","TIMESTAMP");//remove the engine type
+
+			//remove the crap at the top
+			sql = sql.replaceAll("CREATE SCHEMA.*?;","");//remove the engine type
+			sql = sql.replaceAll("USE.*?;","");//remove the engine type
+			sql = sql.replaceAll("SET.*?;","");//remove the engine type
 
 			
-			
+
+	        BufferedWriter writer = new BufferedWriter(new FileWriter("oracle.sql"));
+	        
+	        writer.append(sql);
+	        writer.flush();
 			
 			
 
 		} catch (FileNotFoundException e) {
+			System.err.println("file not found");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.err.println("there was an IO exeption");
 			e.printStackTrace();
 		}
 	}
